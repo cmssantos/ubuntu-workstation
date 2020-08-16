a6823eb7e6ab70a075d628ae8410fb4c82ab1611
Cleudice Marques Santos <cleudice.ms@gmail.com>
2020-08-16 17:26:27 -0300

    Update install

--- ubuntu-workstation.sh
+++ ubuntu-workstation.sh
@@ -35,6 +35,8 @@ sudo apt install fonts-firacode &&
 sudo apt install ubuntu-restricted-extras -y && 
 sudo apt install curl -y &&
 sudo apt install vlc -y &&
+sudo apt install freetux &&
+sudo apt install pavucontrol &&
 
 sudo snap install spotify && 
 sudo snap install code --classic && 
@@ -74,6 +76,21 @@ sudo snap connect remmina:password-manager-service :password-manager-service &&
 
 ## Configurações do Gnome ##
 
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-overview'
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide-in-fullscreen true
+
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 38
+
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'DYNAMIC'
+
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
 
 ## Atualização do sistema ##
 
